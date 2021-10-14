66b5cf4e6e7d06baf5e2e2df8ec24c36a57f9304
Ban Cinti <bncincia@gmail.com>
2021-10-14 09:10:53 +0200

    cs

---
+++ src/app.js
@@ -0,0 +1,15 @@
+var diameterInput = document.querySelector('#diameter');
+var resultInput = document.querySelector('#result');
+var calcButton = document.querySelector('#calcButton');
+function calcArea(diameter) {
+    var radius = diameter / 2;
+    var area = Math.pow(radius, 2) * Math.PI;
+    var areaPart = area / 2;
+    return areaPart;
+}
+calcButton.addEventListener('click', function (event) {
+    var diameter = Number(diameterInput.value);
+    var areaPart = calcArea(diameter);
+    console.log(areaPart);
+    resultInput.value = String(areaPart.toFixed(2));
+});
